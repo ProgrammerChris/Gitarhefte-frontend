d8cb5d9437b06d34e0f4aee91dc91ab1f67860be
ProgrammerChris <chris.tvedt@gmail.com>
2020-06-22 17:22:43 +0200

    Can now delete items from the booklet list, and drag-n-drop! Awesome!

--- src/components/booklet/booklet.js
+++ src/components/booklet/booklet.js
@@ -1,42 +1,104 @@
 import React, { useState, useEffect } from 'react'
-import SelectedSong from './selected/selectedSong';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 
 const Booklet = () => {
-    const [songDeleted, setSongDeleted] = useState(false);
-    const [selectedSongs, setSelectedSongs] = useState(Array.from(JSON.parse(sessionStorage.getItem('booklet'))))
 
-    useEffect(() => {
-        setSelectedSongs(Array.from(JSON.parse(sessionStorage.getItem('booklet')))) // Update the list of selected songs
-        setSongDeleted(false) // Set the changed to false so that it can see if another change has been done.
-    }, [songDeleted])
+    const textStyle = {
+        gridRow: "2",
+        gridColumn: "1/4",
+        textAlign: "center",
+        marginTop: "15px",
+        fontSize: "26px",
+        fontWeight: "Bold",
+        color: "#622C06"
+    }
 
-    const SortableItem = SortableElement(({ song }) =>
-        <SelectedSong isSongDeleted={() => setSongDeleted(true)} songName={song['songName']} artistName={song['artistName']} />
-    );
+    const inputStyle = {
+        gridRow: "1",
+        gridColumn: "1/4",
+        backgroundColor: "#fff",
+        height: "50px",
+        border: "none",
+        textAlign: "center",
+        borderRadius: "15px",
+        boxShadow: "-5px 2px 15px 1px #805020",
+        fontSize: "20px",
+        fontWeight: "bold",
+        outline: "none",
+    }
 
-    const SortableList = SortableContainer(({ songs }) => {
+    const songListStyle = {
+        display: "grid",
+        gridRowGap: "20px",
+        gridRow: "3",
+        padding: "0px",
+        maxHeight: "60vh",
+        overflowY: "auto",
+        overflowX: "hidden",
+        paddingTop: "15px",
+        paddingBottom: "15px",
+        marginTop: "0px",
+    }
 
-        return (
-            <ul style={listStyle}>
-                {songs.map((song, index) => (
-                    <SortableItem key={`id`+index} index={index} song={song} />
-                ))}
-            </ul>
-        );
-    });
+    const buttonListStyle = {
+        display: "grid",
+        gridRowGap: "20px",
+        gridRow: "3",
+        padding: "0px",
+        maxHeight: "60vh",
+        overflowY: "auto",
+        overflowX: "hidden",
+        paddingTop: "15px",
+        paddingBottom: "15px",
+        marginTop: "0px",
+        paddingLeft: "20px"
+    }
 
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-        const newSelectedSongs = arrayMove(selectedSongs, oldIndex, newIndex)
-        setSelectedSongs(newSelectedSongs)
-        sessionStorage.setItem('booklet', JSON.stringify(newSelectedSongs))
-    };
+    const listElementStyle = {
+        display: "grid",
+        marginLeft: "20px",
+        marginRight: "30px",
+
+    }
+
+    const style = {
+        fontSize: "24px",
+        backgroundColor: "#fff",
+        height: "fit-content",
+        maxHeight: "100px",
+        borderRadius: "15px",
+        boxShadow: "-5px 2px 15px 1px #805020",
+        border: "none",
+        textAlign: "center",
+        lineHeight: "50px",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+        width: "100%"
+    }
+
+    const deleteStyle = {
+        gridColumn: "2",
+        justifySelf: "center",
+        marginRight: "10px",
+        width: "45px",
+        height: "45px",
+        backgroundColor: "#622C06",
+        borderRadius: "50%",
+        border: "none",
+        cursor: "pointer",
+        boxShadow: "-5px 2px 15px 1px #805020",
+        outline: "none"
+    }
+    const iconStyle = {
+        width: "25px",
+        //Change color in the SVG fill.
+    }
 
     const buttonStyle = {
         gridRow: "4",
-        gridColumn: "2/3",
-        display: "block",
+        gridColumn: "2",
+        display: "inline-block",
         borderRadius: "15px",
         border: "none",
         backgroundColor: "#fff",
@@ -45,13 +107,54 @@ const Booklet = () => {
         fontSize: "20px",
         height: "50px",
         outline: "none",
-        width: "100%",
+        width: "fit-content",
         cursor: "pointer",
     }
 
-    return (
+    const [selectedSongs, setSelectedSongs] = useState(Array.from(JSON.parse(sessionStorage.getItem('booklet'))))
 
-        <div style={{ display: "grid", gridTemplateColumns: "auto 50% auto", gridColumn: "2/5" }}>
+    const SortableItem = SortableElement(({ song, index }) =>
+        <li style={listElementStyle}>
+            <div style={style}>
+                {song['songName']} by {song['artistName']}
+            </div>
+        </li >
+    );
+
+    const SortableList = SortableContainer(({ songs }) => {
+
+        return (
+            <ul style={songListStyle}>
+                {songs.map((song, index) => (<SortableItem key={`id-${index}`} index={index} song={song} />))}
+            </ul>
+        );
+    });
+
+    const deleteButtons = (song, index) =>
+        <li key={`id-${index}`} style={{ display: "grid" }}>
+            <button type="button" name="corner-button" style={deleteStyle} onClick={() => remove(song)}>
+                <img alt="Hefte knapp" style={iconStyle} src={require('../../img/trash.svg')} />
+            </button>
+        </li>
+
+    const remove = (song) => {
+        const index = selectedSongs.indexOf(song);
+        if (index > -1) {
+          selectedSongs.splice(index, 1);
+        }
+        sessionStorage.setItem('booklet', JSON.stringify(selectedSongs))
+        setSelectedSongs(Array.from(JSON.parse(sessionStorage.getItem('booklet'))))
+        
+    }
+
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+        const newSelectedSongs = arrayMove(selectedSongs, oldIndex, newIndex)
+        setSelectedSongs(newSelectedSongs)
+        sessionStorage.setItem('booklet', JSON.stringify(newSelectedSongs))
+    };
+
+    return (
+        <div id="booklet" style={{ display: "grid", gridColumn: "2/5", gridTemplateColumns: "auto 25% auto" }}>
             <input
                 id="name-input"
                 name="bookletName"
@@ -59,51 +162,15 @@ const Booklet = () => {
                 placeholder="Hefte navn"
                 style={inputStyle} />
             <div style={textStyle}>Valgte sanger</div>
-            <SortableList songs={selectedSongs} onSortEnd={onSortEnd}></SortableList>
-            <button style={buttonStyle} >Last ned hefte</button>
+            <div id="list" style={{ display: "grid", gridRow: "3", gridColumn: "1/4", gridTemplateColumns: "6fr auto", gridTemplateRows: "1" }}>
+                <SortableList songs={selectedSongs} onSortEnd={onSortEnd}></SortableList>
+                <ul style={buttonListStyle}>
+                    {selectedSongs.map((song, index) => deleteButtons(song, index))}
+                </ul>
+            </div>
+            <button style={buttonStyle}>Last ned hefte</button>
         </div>
     )
 }
-
-const textStyle = {
-    gridRow: "2",
-    gridColumn: "2/3",
-    textAlign: "center",
-    marginTop: "15px",
-    fontSize: "26px",
-    fontWeight: "Bold",
-    color: "#622C06"
-}
-
-const inputStyle = {
-    gridRow: "1",
-    gridColumn: "1/4",
-    backgroundColor: "#fff",
-    height: "50px",
-    border: "none",
-    textAlign: "center",
-    borderRadius: "15px",
-    boxShadow: "-5px 2px 15px 1px #805020",
-    fontSize: "20px",
-    fontWeight: "bold",
-    outline: "none",
-}
-
-const listStyle = {
-    display: "grid",
-    gridRowGap: "20px",
-    gridRow: "3",
-    gridColumn: "1/4",
-    padding: "0px",
-    height: "fit-content",
-    maxHeight: "60vh",
-    overflowY: "auto",
-    overflowX: "hidden",
-    paddingTop: "15px",
-    paddingBottom: "15px",
-    marginTop: "0px",
-}
-
-
 
 export default Booklet;
